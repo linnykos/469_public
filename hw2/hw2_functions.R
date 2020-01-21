936c4f356f39982dcf36a566dc2965ad391a927b
Kevin Lin <klin1234@gmail.com>
2020-01-21 16:47:32 -0500

    Added a function for coefficients

--- hw2/hw2_functions.R
+++ hw2/hw2_functions.R
@@ -8,11 +8,24 @@ output_predictions <- function(fit_obj, dat, response_idx){
     pred_vec <-  stats::predict(fit_obj, newx = dat, type = "response")
     pred_vec <- as.numeric(pred_vec >= 0.5)
       
-  } else if("cv.glmnet" %in% class(fit_obj)){
+  } else {
      
     pred_vec <- as.numeric(glmnet:::predict.cv.glmnet(fit_obj, newx = as.matrix(dat[,-response_idx]), 
                                            s = "lambda.1se", type = "class"))
   }
   
   pred_vec
+}
+
+output_coefficients <- function(fit_obj, dat, response_idx){
+  stopifnot(any(c("glm", "cv.glmnet") %in% class(fit_obj)))
+  
+  if("glm" %in% class(fit_obj)){
+    coef_vec <- coef(fit_obj)
+  } else {
+    coef_vec <- as.numeric(stats::coef(fit_obj, s = "lambda.1se"))[-1]
+    names(coef_vec) <- colnames(dat)[-response_idx]
+  }
+  
+  coef_vec
 }
